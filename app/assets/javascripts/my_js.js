7c39d13946ecb7e58a09674bfefbcb5af3b6ada2
Alexander Kazakov <alexander@kazakov.mobi>
2014-04-11 12:22:39 -0700

    Add MixPanel landing page and button click events.

--- app/assets/javascripts/my_js.js
+++ app/assets/javascripts/my_js.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	ZeroClipboard.config( { moviePath: '/assets/ZeroClipboard.swf' } );
 	//var client = new ZeroClipboard($(".promo_code"));
+
+	$('#getAppBtn').on('click', function(e){
+
+		//e.preventDefault();
+		mixpanel.track("Get App Button Click");
+		alert("here");
+	})
 });
